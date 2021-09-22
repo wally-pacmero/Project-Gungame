df374c86b191cce1edd4226ec44b4e864b054e60
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2021-09-22 10:29:07 +1000

    Added more functionality and tests into id.php
    Powered by PHP $_GET, Python sys.argv, and a smart interactive input for testing.
    
    Python string slice updated to [69:420].

--- resources/tiles/id.php
+++ resources/tiles/id.php
@@ -1,4 +1,5 @@
 exec('''<?php
+// $ py id.php works ok
 file_put_contents($f="id.py",
 <<<PYTHON
 #!/usr/bin/env python
@@ -9,8 +10,8 @@ btw all the classes here are just prototypes, not for actual use.
 unless its good enough and actuall .png resource management is added.
 currently it only is checking names and matching them.
 
-# @todo get tile by ID and Name
-# @todo open image from ID or Name
+# @todo [Further tileHandler class testing by Import. But I'd rather
+		spend that time in C# in Unity...]
 """
 
 if "Console colour formatter classes from another project":
@@ -113,6 +114,28 @@ from sys import argv
 from dataclasses import dataclass
 from typing import *
 
+nl = chr(10)
+""" PHP compatibility.
+#	We don't want the PHP interpreter to expand the \n As it could misplace statements after it,
+#	such as the part just above out of a comment into the next line.
+#	(not needed if inside a multiline string)
+
+# ... the \n As it ...
+
+# Into
+
+# ... the
+  As it ... <-- causes exception!
+"""
+
+def get_yaml(file_name: str = "id.yaml") -> str:
+	if not file_name: exit()
+	try:
+		with open(file_name, "r") as f:
+			return f.read()
+	except FileNotFoundError:
+		print(fg.r("id.yaml not found"))
+		return get_yaml(input("Location: "))
 
 class resourceHandler(dict):
 	def byID(self, ID): return self[ID]
@@ -124,17 +147,22 @@ class tile:
 		""" 'loads' image into memory """
 		self.id = id
 		self.name = name
-		self.image_path: str = category_path + "/" + name + ".png"
+		self.path: str = category_path + "/" + name + ".png"
 		#super().__init__() # call the original __init__ of the @dataclass
 		try:
-			with open(self.image_path, "rb") as f:
+			with open(self.path, "rb") as f:
 				self.image = base64.b64encode(f.read())
 		except FileNotFoundError:
 			self.image = None
 
-	def __repr__(self):
-		self.name
+	def __repr__(self) -> str:
+		return self.name
 
+	def __add__(self, other, r=False):
+		if type(other) not in [str, self.__class__]: raise NotImplementedError
+		return (str(other)+self.name) if r else (self.name + str(other))
+	def __radd__(self, other):
+		return self.__add__(other, r=True)
 	__str__ = __repr__
 
 
@@ -154,13 +182,12 @@ class tileCategory(dict):
 		# self.list = [tile(ID, name, data["dir"]) for ID, name in self.list]
 
 		self.tiles: Dict[int, tile] = {}
-		for ID, name in enumerate(data["list"]):
-			ID += id_offset
-			if type(name) == list:
-				self.tiles[ID] = tile(ID, name[0], name[1])
-			else:
-				self.tiles[ID] = tile(ID, name, data["dir"])
-		
+
+	def delete_category(self, name: str) -> None:
+		del self[name]
+	
+	def new_category(self, name: str, data) -> None:
+		self[name] = data
 
 class tileHandler(resourceHandler):
 	def p(s, *_): print(*_)
@@ -169,34 +196,47 @@ class tileHandler(resourceHandler):
 		self.raw: str = raw
 		self.name_list: list = self.id_flatten() # Get name of tile by ID
 		self.category: dict = {}
+		self.tiles: dict = {}
 
 		if not debug:
 			self.p = lambda *_: None
 
-		try:
-			self.data: dict	= yaml.safe_load(raw)
-		except Exception as e:
-			print(e)
-			exit("Invalid yaml")
+		self.data: dict	= yaml.safe_load(raw) # Will raise exception if invalid YAML
 
 		invalid_categories: list = []
-		for name, value in self.data.items():
+		self.missing = []
+		for category, entry in self.data.items():
+			# length of all previous lists combined
+			offset = sum([i.list for i in list(self.category.values())], []).__len__()
 			try:
-				offset = sum([i.list for i in list(self.category.values())], []).__len__()
-				self.category[name] = tileCategory(offset, value)
+				self.category[category] = tileCategory(offset, entry)
 			except KeyError:
 				# KeyError from self.dir: str = data["dir"] from tileCategory.__init__
-				invalid_categories.append(name)
-		if invalid_categories: exit(
-			"There are categories with invalid structures."+chr(10)+
-			"Bad categories:"+chr(10)*2+ chr(10).join(invalid_categories)
-		)
+				invalid_categories.append(category)
+			else:
+				for ID, tile_name in enumerate(entry["list"]):
+					ID += offset
 
-		self.missing = self.load_all()
+					# Adds new tile object to both tileHandler.tiles and category tiles
+					self.tiles[ID] = \
+					self.category[category].tiles[ID] = \
+					\
+					tile(ID, tile_name[0], tile_name[1]) \
+					if type(tile_name) == list else \
+					tile(ID, tile_name, entry["dir"])
+
+					if self.tiles[ID].image is None:
+						self.missing.append(self.tiles[ID].path)
+
+		if invalid_categories: exit(
+			"There are categories with invalid structures."+nl+
+			"Bad categories:"+nl*2+nl.join(invalid_categories)
+		)
 
 
 	def id_flatten(self) -> Dict[int, str]:
 		""" enumerates all ids from the top of the .yaml file """
+
 		# return [
 		# 	line[5:].strip() for line in self.raw.split(chr(10))
 		# 	if line[:5] == "    -"
@@ -204,7 +244,7 @@ class tileHandler(resourceHandler):
 		
 		tiles = []
 
-		for line in [line for line in self.raw.split(chr(10)) if line[:5] == "    -"]:
+		for line in [line for line in self.raw.split(nl) if line[:5] == "    -"]:
 			line = yaml.safe_load(line[5:])
 			tiles.append(
 				line[1] if type(line) is list else line
@@ -214,14 +254,19 @@ class tileHandler(resourceHandler):
 
 
 	def load_all(self) -> list:
-		""" 'loads' all resources, Empty list on success """
+		"""
+		'loads' all resources, Empty list on success
+		Scans for "    -" in yaml
+		"""
+		self.p(bg.b(fg.W(nl+"--LOADING ALL--")))
+		self.p(fg.Y("Prepended ")+fg.r("exclamation mark")+fg.Y(" if missing"))
 		missing = []
+		ID = 0
 		for name, cat in self.category.items():
 			self.p(fg.y(name))
 			self.p(" at dir: "+fg.B(cat.dir+"/"))
 
 			for tile in cat.list:
-				if tile == "air": continue
 
 				loc = '"'+cat.dir+"/"
 				loc += tile if type(tile) is str else (tile[0]+"/"+tile[1])
@@ -229,17 +274,19 @@ class tileHandler(resourceHandler):
 
 				if not (path.isfile(loc.strip('"'))):
 					# raise FileNotFoundError(loc)
-					missing.append(loc)
-					self.p(fg.r("! \t"+loc))
+					missing.append(loc.strip('"'))
+					self.p(fg.r("! ")+str(ID)+fg.r("\t"+loc))
 				else:
-					self.p("\t"+fg.g(loc))
-		
+					self.p("  "+str(ID)+"\t"+fg.g(loc))
+
+				ID += 1
+		self.p(bg.b(fg.W(nl+"--LOAD ALL FINISH--")))
 		return missing
 
 
 	def print_missing(self) -> bool:
 		""" Print out missing tiles from load_all. True for OK """
-		if self.missing: self.p(chr(10)+"missing: ")
+		if self.missing: self.p(nl+"missing: ")
 		else: self.p("All tiles found")
 		for i in self.missing:
 			self.p(i)
@@ -247,54 +294,96 @@ class tileHandler(resourceHandler):
 		return not bool(self.missing)
 
 
-def interactive(tileHandler: tileHandler) -> None:
+	def report_all(self) -> bool:
+		""" True once everything is OK """
+		return (not self.missing == self.load_all()) and not self.missing
+
+
+
+def interactive(tileHandler: tileHandler, yaml_location, original, _inp: list=None) -> None:
 	""" Interactive tile testing """
 
-	def open_img(ID: int):
-		return NotImplemented
-		Image.open(BytesIO(ID)).show()
+	def open_img(image):
+		# return NotImplemented
+		Image.open(image.path).show()
 
-	inp = "help"
-	while inp:
-		inp = inp.strip()
+	user: list = ["help"] if _inp is None else _inp[:0:-1] + ["help"]
+	arged: bool= bool(_inp)
+
+	if not arged: print(nl+fg.m("Entering Interactive Mode"))
+
+	# List contains elements and the next one is not empty
+	while user and user[-1].strip(): # user and (inp := user.pop.strip())
+		inp = user.pop().strip()
+
 		if "exit" in inp:
 			exit()
+
 		elif inp in ["help", "help()", "?", "/?","/help",
 			"-?", "-help", "--?", "--help"]:
-			print(chr(10)+"type number or name of tile to open the image")
-			print("type exit or nothing to quit")
+			print(
+				"Type number or name of tile to open the image" +
+				nl + "Type exit or nothing to quit"
+			)
+
+
 		elif inp.isdigit():
 			try:
-				print("Tile name: "+tileHandler.name_list[int(inp)])
+				print("Tile name of ID "+inp+": "+tileHandler.tiles[int(inp)])
 			except IndexError:
 				print("IndexError, Tile not found")
 				continue
-			if input("Type anything to show "):
-				open_img(int(inp))
+			if tileHandler.tiles[int(inp)].image is None:
+				print("Image Missing")
+			elif not arged and input("Type anything to show "):
+				open_img(tileHandler.tiles[int(inp)])
+
+		elif "check" in inp:
+			if arged or input("Type anything to reload data "):
+				tileHandler = original(get_yaml())
+			print("Everything is OK" if tileHandler.report_all() else
+				fg.Y("There are errors or data missing"))
+
+		elif inp == "php": pass
 		else:
-			print("Unknown command: "+inp)
-		inp = input(fg.M("> "))
+			print("Unknown command: " + inp)
+
+		if not arged: user = [input(fg.M("> "))]
 
 if __name__ == "__main__":
-	try:
-		with open("id.yaml", "r") as f:
-			raw = f.read()
-	except FileNotFoundError:
-		print("id.yaml missing")
+	raw = get_yaml()
+	
+	_tileHandler = tileHandler(raw, debug=pprint)
 
-	tileHandler = tileHandler(raw, debug=pprint)
+	if pprint: print(
+		nl+"Flattened into: "+nl+
+		"{"+nl+
+		(","+nl).join(["  {}: \\"{}\\"".format(str(i), j)for
+			i,j in _tileHandler.tiles.items()])
+		+nl+"}"
+	)
 
-	if pprint: print(chr(10)+"Flattened into: ")
-	pprint({i:j for i,j in enumerate(tileHandler.name_list)})
+	_tileHandler.report_all()
 
 	# todo: display tileHandler.category stuff and more
 
-	print(fm["u"](fg.G(chr(10)+"test successful!")))
-
-	if "php" not in argv: interactive(tileHandler)
+	print(fm["u"](fg.G(nl+"test finished!")))
+	interactive(
+		_tileHandler,
+		raw,
+		original=tileHandler,
+		_inp = None if len(argv) < 2 else argv
+	)
+	del _tileHandler
 
 PYTHON
 );
-echo '<pre>'.preg_replace("/\033\[\t*./m", "", shell_exec("python ".$f." php"))."</pre>";
+// $_GET >> SHELL EXEC EXTREMELY DANGEROUS IF YOU HOST THIS ONLINE!!! localhost please!
+// ?? is PHP>=8.0?
+echo '<pre>'.preg_replace("/\033\[\d*./m","",shell_exec("python ".$f." php ".$_GET["args"] ?? ""))."</pre>";
 unlink(realpath($f));
-?><style>body{color:transparent;user-select:none;}pre{color:initial;user-select:text;}</style>'''[46:-232])
+?><style>body{color:transparent;user-select:none;}pre{color:initial;user-select:text;}</style><form method="GET"><input name="args" placeholder="args, enter to submit"></form>'''[
+
+69:420
+
+*-1])
