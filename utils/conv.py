e8224eddc0b4a951f843ce9cfebf62547c42eb89
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2022-11-28 17:06:49 +1100

    can convert yaml to json with no comments now

--- utils/conv.py
+++ utils/conv.py
@@ -6,14 +6,22 @@ import json5
 import yaml
 
 print("You have to manually put the comments back in :(")
-conv = input("type 0 to convert all json to yaml, type 1 to convert all yaml to json: ") == "0"
+
+# Checking input
+conv = input("type 0 to convert all json to yaml, type 1 to convert all yaml to json: ")
+if conv in ["0", "1"]:
+	conv = conv == "0"
+else:
+	print("Invalid option")
 
 # Check if in pgg folder
 if path.isdir("../resources"):
 	print("found resources folder")
 else:
-	print("resources not found! Is this file in the Project-Gungame/util directory?\nAborting")
+	print("resources not found! Is this file in the Project-Gungame/utils directory?\nAborting")
 	exit()
+
+keep = input("keep comments? (Y/n): ") != "n"
 
 for directory in walk("../resources"):
 	for file_name in directory[2]:
@@ -22,11 +30,14 @@ for directory in walk("../resources"):
 				fp = directory[0] + "\\" + file_name
 				print("converting", fp)
 				with open(fp, "r") as f:
-					comments = [i.replace("//", "#", 1) for i in f.readlines() if i.lstrip().startswith("//")]
+					comments = "".join([i.replace("//", "#", 1) for i in f.readlines() if i.lstrip().startswith("//")])
 					f.seek(0)
 					data = json5.loads("".join([i for i in f.readlines() if not i.lstrip().startswith("//")]))
 				with open(fp, "w") as f:
-					yaml.dump(data, f, sort_keys=False)
+					if keep:
+						f.write(comments + ("\n" if comments else "") + yaml.dump(data, sort_keys=False))
+					else:
+						f.write(yaml.dump(data, sort_keys=False))
 				rename(fp, sub(r"\.json$", ".yaml", fp))
 
 		else:
@@ -38,5 +49,8 @@ for directory in walk("../resources"):
 					f.seek(0)
 					data = yaml.safe_load(f)
 				with open(fp, "w") as f:
-					f.write(comments + ("\n" if comments else "") + json5.dumps(data, indent=4, sort_keys=False))
+					if keep:
+						f.write(comments + ("\n" if comments else "") + json5.dumps(data, indent=4, sort_keys=False))
+					else:
+						json5.dump(data, f, indent=4, sort_keys=False)
 				rename(fp, sub(r"\.yaml$", ".json", fp))
